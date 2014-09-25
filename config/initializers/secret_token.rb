9f332c516fc6f0818868d5fbe418bcdbe39b1ce7
prakash0450 <prakash0450@yahoo.com>
2014-09-25 10:06:13 -0400

    first checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeanRails::Application.config.secret_key_base = 'f10cdf7b74c8af75049987274382dc05899e14d7fa4e05f76fed9eef7f7de9e020a199029c7bfb171c3f6a472a17864c82091e25cb3258db18c7600754a073c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LeanRails::Application.config.session_store :cookie_store, key: '_lean_rails_session'
