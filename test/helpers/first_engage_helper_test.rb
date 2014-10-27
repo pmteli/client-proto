41925a3bac74627e75d2f2b9751ce088c8bb5a0e
Prakash Teli <pteli@firstfuel.com>
2014-10-27 12:17:15 -0400

    first check-in

---
+++ test/helpers/first_engage_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class FirstEngageHelperTest < ActionView::TestCase
+end
---
+++ test/models/ecm_catalog_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EcmCatalogTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
